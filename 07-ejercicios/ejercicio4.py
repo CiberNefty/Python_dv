2d66b176a088a0f1f609a8227b6b8b1bacfed95e
Daniel Santiago <danieldbaproyects@gmail.com>
2022-04-01 22:19:25 -0500

    Ejercicio 4 realizado por DanielVera agrego linea 26 a la 40, segundo commit de este archivo

--- 07-ejercicios/ejercicio4.py
+++ 07-ejercicios/ejercicio4.py
@@ -5,6 +5,7 @@ de una calculadora y mostrarla por pantalla
 """
 # Ejercicio por Daniel Vera
 print("Calculadora por numeros\n\t")
+"""
 num1= int(input("Ingresa el primero numero: "))
 num2= int(input("Ingresa el segundo numero: "))
 
@@ -21,3 +22,21 @@ f"El resultado de la multiplicacion es: {multi}\n"
 f"El resultado de la division es: {divi}\n"
 f"El resultado del resto es: {resto}\n"
 f"El resultado de la exponeciasion es: {expo}");
+"""
+pregunta = 0
+while pregunta < 3:
+    
+    if pregunta == 1:
+        num1 = int(input("Ingresa el primer numero: "))
+    elif pregunta == 2:
+        num2 = int(input("Ingresa el segundo numero: "))
+    pregunta += 1
+    
+print(f"El resultado de la suma es: {num1 + num2}\n"
+f"El resultado de la resta es: {num1 - num2}\n"
+f"El resultado de la multiplicacion es: {num1 * num2}\n"
+f"El resultado de la division es: {num1 / num2}\n"
+f"El resultado del resto es: {num1 % num2}\n"
+f"El resultado de la exponeciasion es: {num1 ** num2}");
+
+#-------------------------------------------------------------

77d1f3ef01bc7916b05fe126550d7e46f21bec49
Daniel Santiago <danieldbaproyects@gmail.com>
2022-04-04 10:45:07 -0500

    Creacion de ejercicio 5

---
+++ 07-ejercicios/ejercicio5.py
@@ -0,0 +1,18 @@
+"""
+ejercicio 5.
+Hacer un programa que muesttre todos los numeros entre 
+dos numero que diga el usuario
+"""
+num1 = int(input("Ingrese primer numero: "))
+num2 = int(input("Ingrese segundo numero: "))
+"""
+for num1 in range(num1,num2):
+    print(num1)
+"""
+#####################################
+# Horizontal Mente los muestros
+num = str(0)
+for num1 in range(num1,num2):
+    num = num + ", "+str(num1)
+    
+print(num)    

74874e54264a919db97003461eca3ffc12028ac1
Daniel Santiago <danieldbaproyects@gmail.com>
2022-04-04 11:27:31 -0500

    Creacion del ejercicio 6, (tablas de multiplicar) primer commit

---
+++ 07-ejercicios/ejercicio6.py
@@ -0,0 +1,48 @@
+"""
+Ejercicio 6.
+Mostrar todas las tablas de multiplicar del 1 al 10 
+Mostrar el titulo de las tablas y luego las multiplicaciones.
+"""
+
+for num in range(1,(10+1)):
+    if num == 1:
+        print("### TABLA DEL 1")
+        for i in range(1,11):
+            print(f"{num} X {i} = {num*i}")
+    elif num == 2:
+        print("### TABLA DEL 2")
+        for i in range(1,11):
+            print(f"{num} X {i} = {num*i}")
+    elif num == 3:
+        print("### TABLA DEL 3")
+        for i in range(1,11):
+            print(f"{num} X {i} = {num*i}")
+    elif num == 4:
+        print("### TABLA DEL 4")
+        for i in range(1,11):
+            print(f"{num} X {i} = {num*i}")
+    elif num == 5:
+        print("### TABLA DEL 5")
+        for i in range(1,11):
+            print(f"{num} X {i} = {num*i}")
+    elif num == 6:
+        print("### TABLA DEL 6")
+        for i in range(1,11):
+            print(f"{num} X {i} = {num*i}")
+    elif num == 7:
+        print("### TABLA DEL 7")
+        for i in range(1,11):
+            print(f"{num} X {i} = {num*i}")
+    elif num == 8:
+        print("### TABLA DEL 8")
+        for i in range(1,11):
+            print(f"{num} X {i} = {num*i}")
+    elif num == 9:
+        print("### TABLA DEL 9")
+        for i in range(1,11):
+            print(f"{num} X {i} = {num*i}")
+    elif num == 10:
+        print("### TABLA DEL 10")
+        for i in range(1,11):
+            print(f"{num} X {i} = {num*i}")
+    

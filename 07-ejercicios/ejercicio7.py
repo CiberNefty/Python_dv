d815272e983b4e26554fe5af1ffbc7b528459a98
Daniel Santiago <danieldbaproyects@gmail.com>
2022-04-04 12:25:16 -0500

    Modificamos el archivo ejercicio 7

--- 07-ejercicios/ejercicio7.py
+++ 07-ejercicios/ejercicio7.py
@@ -5,9 +5,19 @@ entre dos numero que decida el usuario.
 """
 numero1 = int(input("Ingresa primer numero: "))
 numero2 = int(input("Ingresa segundo numero: "))
+"""
 pared =str(0)
 for contador in range (numero1, (numero2+1)):
     if contador % 2 == 1:
         pared = pared + ", "+str(contador)
 print(pared)
-        
+"""
+#EJEMPLO por victor
+if numero1 < numero2:
+    for x in range (numero1,(numero2+1)):
+        if x%2 == 0:
+            print(f"{x} es PAR")
+        else:
+            print(f"{x} es IMPAR")
+else: 
+    print(f"El numero {numero1} tiene que ser menor al numero {numero2}")

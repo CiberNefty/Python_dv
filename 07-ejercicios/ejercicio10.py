01fca9a752193fdca8061d8ef723fd1917f162fa
Daniel Santiago <danieldbaproyects@gmail.com>
2022-04-06 15:13:51 -0500

    Modificamos el ejejercicio 10 agregregando otro ejercicio, v2

--- 07-ejercicios/ejercicio10.py
+++ 07-ejercicios/ejercicio10.py
@@ -26,3 +26,21 @@ for i in range(1,4):
 print(f"Estudiantes aprobados: {aprobar}")
 print(f"Estudiantes Reprobados: {reprobar}")
 #---------------------------------------------------------------
+#Ejercicio por Vic
+contador = 1
+aprobados = 0
+suspendidos = 0
+
+numero_alumnos = int(input("Cuantos alumnos tienes?: "))
+
+while contador <= numero_alumnos:
+    nota1 = int(input(f"Que nota quieres ponerle al \"alumno {contador}\"?: "))
+    
+    if nota1 >= 5:
+        aprobados+= 1
+    else:
+        suspendidos +=1
+    contador += 1
+
+print(f"\nAlumnos aprobado: {aprobados}")
+print(f"Alumnos suspendidos: {suspendidos}")

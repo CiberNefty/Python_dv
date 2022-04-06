07f7d98538b60e1ae457b102378c9c7ef8544734
Daniel Santiago <danieldbaproyects@gmail.com>
2022-04-06 15:47:41 -0500

    Creamos una carpeta de Funciones, v1

---
+++ 08-funciones/main.py
@@ -0,0 +1,30 @@
+"""
+FUNCIONES:
+Una funcion es un conjunto de instrucciones agrupadas bajo un nombre
+concreto que pueden reutilizarse invocando a la funcion 
+tantas veces como sea posible.
+
+def nameMyfuncion(parametros):
+    # Bloque / conjunto de instrucciones
+
+nameMyfuncion(mi_parametro)
+nameMyfuncion(mi_parametro)
+"""
+# Ejemplo 1
+print("####### EJEMPLO 1 ########")
+
+# Definir funcion
+def muestraNombres():
+    print("Daniel")
+    print("Paco")
+    print("elsa")
+    print("Thor")
+    print("Nestro")
+    print("Pipe")
+    print("Felipe")
+    print("\n")
+
+# Invocar funcion
+muestraNombres()
+muestraNombres()
+muestraNombres()

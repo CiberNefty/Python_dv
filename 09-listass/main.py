3261dfc3988ab545e4e0b60c14c009ea8f17c200
Daniel Santiago <danieldbaproyects@gmail.com>
2022-04-09 17:20:29 -0500

    Creamos directorio de Listas y creamos 1 archivo, v1

---
+++ 09-listass/main.py
@@ -0,0 +1,19 @@
+""" 
+LISTAS (arras)
+Son condicionales o conjuntos de datos/valores, bajo un unico nombre.
+Para acceder a esos valores podemos ysar un indice numerico.
+"""
+
+peilcula = "Batman"
+
+# Definir Lista
+peliculas = ["Batman", "Spiderman","El señor de los anillos"]
+cantantes = list(('2pac','Daniel Vera','Amy winhouse'))
+years = list(range(2020,2050))
+variada= ["Vic",34, 34,3, True,"Texto"], 
+
+
+print(peliculas)
+print(cantantes)
+print(years)
+print(variada)

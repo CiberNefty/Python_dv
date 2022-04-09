30ff8e4384a3f3790d645e9fe8db397933587cec
Daniel Santiago <danieldbaproyects@gmail.com>
2022-04-09 17:30:49 -0500

    Agregmos el como acceder y modificar un indice, v2

--- 09-listass/main.py
+++ 09-listass/main.py
@@ -12,8 +12,25 @@ cantantes = list(('2pac','Daniel Vera','Amy winhouse'))
 years = list(range(2020,2050))
 variada= ["Vic",34, 34,3, True,"Texto"], 
 
-
+"""
 print(peliculas)
 print(cantantes)
 print(years)
-print(variada)
+print(variada) """
+#---------------------------------
+
+# Indice
+
+#Acceder a un indice de la lista
+print(peliculas[1])
+print(peliculas[-2])
+print(cantantes[1:3])
+print(peliculas[1:])
+
+# Modificar
+pelicula = "Otra cosa"
+peliculas[1] = "Gran Torino"
+peliculas[2] = "El hobbit"
+print(peliculas)
+
+

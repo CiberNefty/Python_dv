00447c02e860bb26c554cf856450ab6531f28ffc
CiberNefty <danieldbaproyects@gmail.com>
2022-04-26 16:52:17 -0500

    Agregamos otra solucion al ejercicio, v3

--- 11-ejercicios/ejercicio2.py
+++ 11-ejercicios/ejercicio2.py
@@ -6,7 +6,7 @@ Plus: Usar While y For.
 listanumeros = []
 entrada=""
 contador = 0
-
+"""
 while contador in range(4):
     entrada = input("Ingrese numero a la lista: ")
     listanumeros.append(entrada)
@@ -18,4 +18,22 @@ for i in range(4):
     entrada1 = input("Ingrese valor a la lista: ")
     listanumeros1.append(entrada1)
 
-print(listanumeros1)
+print(listanumeros1)"""
+# ------------------------------
+# Ejercicio por CIV
+
+coleccion = []
+x = 0
+while x < 120:
+    coleccion.append(f"Elementos-{x}")
+    print("Mostrando el: " + coleccion[x])
+    x += 1
+print(coleccion[64])
+"""
+coleccion = []
+for contador1 in range(0,120):
+    coleccion.append(f"Elemento-{contador1}")
+    print("Mostrando el: "+ coleccion[contador1])
+
+print(coleccion[115]) # Si queremos sacar una posicion en concreto
+"""

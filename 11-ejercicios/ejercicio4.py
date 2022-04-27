ac4e3d4588b41ad81b6893ff1a893b56abe35688
CiberNefty <danieldbaproyects@gmail.com>
2022-04-27 14:45:38 -0500

    Agregamos otra funcion en forma de evaluar el tipo y mostrar esa funcion dentro de la funcion principal, v3

--- 11-ejercicios/ejercicio4.py
+++ 11-ejercicios/ejercicio4.py
@@ -4,12 +4,25 @@ Crear un script que tenga 4 variables, una lista, un string,
 un entero y un booleano y que imprima un mensaje 
 segun el tipo de dato de cada varibale.
 """
+def traducirTipo(tipo): # cuando le pasemos un tipo de dato, me devuelva segun el tipo de dato que sea.
+    result = None
+    if tipo  == list:
+        result= 'LISTA'
+    elif tipo == str:
+        result = 'CADENA DE TEXTO'
+    elif tipo == int:
+        result = 'NUMERO ENTERO'
+    elif tipo == bool:
+        result = 'BOOLEANO' 
+    
+    return result
+
 def comprobar_tipado(dato, tipo): # Aqui ya estaria realizado el ejercicio  pero necesitamos que cuando imprima deje de imprimir <class 'tipo'>, necesitamos algo mas realisa nos crearemos una nueva funcion.
     test = isinstance(dato, tipo)
     result = ""
 
     if test:
-        result= f"Este dato es del tipo de {type(dato)}"
+        result= f"Este dato es del tipo de {traducirTipo(tipo)}"
     else: 
         result= "El tipo de dato no es correcto"
 

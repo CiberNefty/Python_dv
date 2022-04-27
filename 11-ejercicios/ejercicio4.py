dc7b5c4f5be618ed90e2a571a77ee4bebebf7b4e
CiberNefty <danieldbaproyects@gmail.com>
2022-04-27 14:14:58 -0500

    Creamos archivo ejercicio 4 y le llevamos un seguimiento, v1

---
+++ 11-ejercicios/ejercicio4.py
@@ -0,0 +1,15 @@
+"""
+Ejercicio 4. 
+Crear un script que tenga 4 variables, una lista, un string,
+un entero y un booleano y que imprima un mensaje 
+segun el tipo de dato de cada varibale.
+"""
+lista = [2]
+string = ""
+entero = 0
+booleano = False
+
+print(type(lista),"\n",
+type(string),"\n",
+type(entero),"\n",
+type(booleano),"\n")

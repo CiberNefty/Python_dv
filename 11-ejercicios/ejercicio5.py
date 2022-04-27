5d929f29fbf365585a64664bccb048cd98326482
CiberNefty <danieldbaproyects@gmail.com>
2022-04-27 15:29:04 -0500

    Creacion de archivo ejercicio 5, v1

--- 11-ejercicios/ejercicio5.py
+++ 11-ejercicios/ejercicio5.py
@@ -9,3 +9,23 @@ PUGB        Prince of persia    MOTO GP 21
 - Guardarla en un listado de disccionarios
 - Mostrar esta informacion ordenada
 """
+tablaJuegos = [
+    {
+        'Accion': 'GTA',
+        'Accion': 'COD',
+        'Accion': 'PUGB',
+    },
+    {
+        'AVENTURA': 'ASSASINS',
+        'AVENTURA': 'CRASH',
+        'AVENTURA': 'Prince of persia'
+    },
+    {
+        'DEPORTES': 'FIFA 21',
+        'DEPORTES': 'PRO 21',
+        'DEPORTES': 'MOTO GP 21'
+    }
+
+]
+for tablaGames in tablaJuegos:
+    print(f"ACCION\n{tablaGames['ACCION']['AVENTURA']}")

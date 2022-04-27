60b7a201c0e759526e0eb24bfc5c67ca02a77315
CiberNefty <danieldbaproyects@gmail.com>
2022-04-27 15:11:58 -0500

    Creacion de archivo ejercicio 5, v1

---
+++ 11-ejercicios/ejercicio5.py
@@ -0,0 +1,11 @@
+"""
+Ejercicio5.
+Crear una lista con el contenido de esta tabla:
+ACCION      AVENTURA           DEPORTES
+GTA         ASSASINS            FIFA 21    
+COD         CRASH               PRO 21
+PUGB        Prince of persia    MOTO GP 21
+
+- Guardarla en un listado de disccionarios
+- Mostrar esta informacion ordenada
+"""

6110799a31d2293a9fc19f4ecdcfd1d4bfc378fa
CiberNefty <danieldbaproyects@gmail.com>
2022-05-09 16:47:35 -0500

    Creamos la carpeta modulos 12 que contiene un archivo main, v1

---
+++ 12-modulos/main.py
@@ -0,0 +1,8 @@
+"""
+Modulos: son funcionalidades ya hechas para reutilizar.
+en python hay muchos modulos, que los puedes consultar aqui:
+https://docs.python.org/3/tutorial/modules.html
+
+Podemos conseguir modulos que ya vienen en el lenguaje,
+modulos en internet, y tambien podemos crear nuestros modulos.
+"""

cac420f872c57290ab36f51f95b7217ec759d20b
CiberNefty <danieldbaproyects@gmail.com>
2022-05-09 16:57:05 -0500

    Agregamos lineas de instrucciones y importamos el modulo que acabamos de crear y asu vez invocamos una funcion, v2

--- 12-modulos/main.py
+++ 12-modulos/main.py
@@ -6,3 +6,21 @@ https://docs.python.org/3/tutorial/modules.html
 Podemos conseguir modulos que ya vienen en el lenguaje,
 modulos en internet, y tambien podemos crear nuestros modulos.
 """
+"""
+Para crear un modulo tenemos que crear un archivo dentro de la
+ carpeta de nuestro proyecto y le ponemos el nombre que queramos.
+  <file.py> 
+
+Dentro del archivo vamos a crear las funciones las clases o lo que queramos.
+
+Esos modulos son para crear funciones o programas que utilizare mas adelante.
+
+Puedo utilizar mis modulos llengo al programa principal y escribo 
+la palabra import mi modulo
+"""
+
+# Importar modulo propio
+import mimodulo
+
+# Invocar modulo
+print(mimodulo.holamundo('Daniel Vera'))

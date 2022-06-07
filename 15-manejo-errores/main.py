2b3ee1d3299f7781b4843ccb421ba92c646fd715
CiberNefty <danieldbaproyects@gmail.com>
2022-06-07 16:39:23 -0500

    Agregamos la estructura para manejar multiples excepciones, v3

--- 15-manejo-errores/main.py
+++ 15-manejo-errores/main.py
@@ -2,7 +2,7 @@
 # susceptible a fallos/errores
 
 
-try:
+"""try:
     nombre = input("¿Cual es tu nombre: ")
 
     if len(nombre) > 1:
@@ -15,3 +15,15 @@ else:
     print('Todo ha funcionado correctamente.')
 finally:
     print('Fin de la iteracion !!')
+    """
+# Multiples ecepciones
+try:
+    numero = int(input('Numero para elevarlo al cuadrado: '))
+    print('El cuadrado es: '+str(numero*numero))
+except TypeError:
+    print('Debes convertir tus cadenas a enteros en el codigo')
+#except ValueError:
+    #print('Introduce un numero correcto.')
+except Exception as e: # Aqui eestamos guardando la excepcion en una variable
+    print(type(e))
+    print('Ha ocurrido un error: ',type(e).__name__) # la constante que es .__name__ de esta manera me mostrara el error que hay.

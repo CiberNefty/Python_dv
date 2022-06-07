de578240e066b6e9559a189f0158dc1c2543eb63
CiberNefty <danieldbaproyects@gmail.com>
2022-06-07 16:12:01 -0500

    Agregamos la intruccion principal de commo se debe realizar unmanejo de errores, v2

--- 15-manejo-errores/main.py
+++ 15-manejo-errores/main.py
@@ -1,3 +1,17 @@
 # como capturar excepciones y manjear errores en codigo 
 # susceptible a fallos/errores
 
+
+try:
+    nombre = input("¿Cual es tu nombre: ")
+
+    if len(nombre) > 1:
+        nombre_usuario = 'El nombre es '+ nombre
+    print(nombre_usuario)
+except:
+    print('Ha ocurido un error, mete bien el nombre.')
+
+else:
+    print('Todo ha funcionado correctamente.')
+finally:
+    print('Fin de la iteracion !!')

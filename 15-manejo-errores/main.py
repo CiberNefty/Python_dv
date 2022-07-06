3489eac84529fa86d9ee50360ac96bf4c2a44dc6
CiberNefty <danieldbaproyects@gmail.com>
2022-07-06 16:18:00 -0500

    Agregamosla forma de como podemos manejar multiples excepciones, v3

--- 15-manejo-errores/main.py
+++ 15-manejo-errores/main.py
@@ -1,7 +1,7 @@
 # Capturar Excepcione y manejar errores en codigo 
 # Suceptible a fallos / errores
 
-try:
+"""try:
     nombre = input("cual es tu nombre Nombre?: ")
 
     if len(nombre) > 1: # Aqui le estamos diciendo que si nombre tiene una longitud mayor que 1, entra y guarda ese valor en una nueva variable.
@@ -13,5 +13,17 @@ except: # Esta es el manejo de excepciones.
 else: # La instruccion else en manejo de excepciones nos brindara algo siempre y cuando algo no falle
     print('Todo a funcionado correctamente')
 finally: # la Instruccion finally siempre se va a ejecutar
-    print('FIn de la iteracion')
-    
+    print('FIn de la iteracion')"""
+
+# MULTIPLES EXCEPCIONES
+try:
+    numero = int(input('Numero para elevarlo al cuadrado: '))
+    print("El cuadrado es: "+str(numero*numero))
+except TypeError: #Aqui capturamos la excepcion que queremos.
+    print('Debes convertir tus cadenas en enteros en el cogio!!')
+#except ValueError:
+#    print('Intruduce un numero correcto. !!')
+    # Podemos capturar los errores para que se muestren de una manera mas amigable
+except Exception as e: # Podemos guardar una excepcion en una variable luego despues del as.
+    print(type(e)) # Podemos mostrar la excepcion con type(nombre de la variable que contiene el error.)
+    print('Ha ocurrido un error: ',type(e).__name__)

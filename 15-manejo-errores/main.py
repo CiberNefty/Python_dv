f3ddd9600af4e4512daf645a7246f7368ca6131e
CiberNefty <danieldbaproyects@gmail.com>
2022-07-06 15:55:01 -0500

    Se ecribio un ejercicio simple sonre como se maneja una extructura de errores por si algo falla, v2

--- 15-manejo-errores/main.py
+++ 15-manejo-errores/main.py
@@ -1,5 +1,17 @@
 # Capturar Excepcione y manejar errores en codigo 
 # Suceptible a fallos / errores
 
-nombre = input("cual es tu nombre Nombre?: ")
-print(nombre)
+try:
+    nombre = input("cual es tu nombre Nombre?: ")
+
+    if len(nombre) > 1: # Aqui le estamos diciendo que si nombre tiene una longitud mayor que 1, entra y guarda ese valor en una nueva variable.
+        nombre_usuario = 'El nombre es '+ nombre
+
+    print(nombre_usuario)
+except: # Esta es el manejo de excepciones.
+    print('Ha ocurrido un error, mete bien el nombre.') 
+else: # La instruccion else en manejo de excepciones nos brindara algo siempre y cuando algo no falle
+    print('Todo a funcionado correctamente')
+finally: # la Instruccion finally siempre se va a ejecutar
+    print('FIn de la iteracion')
+    

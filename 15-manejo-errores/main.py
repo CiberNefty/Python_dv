4239f7e2b126608b90eb95cd2b1454da0d73a616
CiberNefty <danieldbaproyects@gmail.com>
2022-07-06 16:39:48 -0500

    Aprendimo un poco de como personalizar excepciones o lanzar excepciones, v4

--- 15-manejo-errores/main.py
+++ 15-manejo-errores/main.py
@@ -16,7 +16,7 @@ finally: # la Instruccion finally siempre se va a ejecutar
     print('FIn de la iteracion')"""
 
 # MULTIPLES EXCEPCIONES
-try:
+"""try:
     numero = int(input('Numero para elevarlo al cuadrado: '))
     print("El cuadrado es: "+str(numero*numero))
 except TypeError: #Aqui capturamos la excepcion que queremos.
@@ -27,3 +27,20 @@ except TypeError: #Aqui capturamos la excepcion que queremos.
 except Exception as e: # Podemos guardar una excepcion en una variable luego despues del as.
     print(type(e)) # Podemos mostrar la excepcion con type(nombre de la variable que contiene el error.)
     print('Ha ocurrido un error: ',type(e).__name__)
+"""
+
+# EXCEPCIONES PERSONALIZADAS O LANZAR EXCEPCIONES
+try:
+    nombre = input("Introduce el nombre: ")
+    edad = int(input("Introduce la edad: "))
+
+    if edad < 5 or edad > 110:
+        raise ValueError("La edad introducida no es real")
+    elif len(nombre) <= 1:
+        raise ValueError("El nombre no esta completo")
+    else:
+        print(f"Bienvenido al Master en Python {nombre} !!")
+except ValueError:
+    print("Introduce los datos correctamente.")
+except Exception as e:
+    print("Existe un error: ", e)

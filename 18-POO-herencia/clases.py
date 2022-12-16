239cc90018fa98fb456f3aed448cae8945746c37
CiberNefty <danieldbaproyects@gmail.com>
2022-12-16 09:40:02 -0500

    Creamos una carpeta nueva la cual se llama 18-POO-herencia el cual cremos dos archivos explicando, v1 - rama master

---
+++ 18-POO-herencia/clases.py
@@ -0,0 +1,10 @@
+# HERENCIA
+"""
+- La herencia es la capacidad de compartir atributos y, metodos entre clases
+de forma que yo pueda tener una clase padre que sea un objeto estandar y luego
+puedo tener diferentes clases hijas que hereden los metodos y atributos de la 
+clase padre y, que sean objetos similares pero con ciertas diferencias.
+- Es la posibilidad de compartir atributos y metodos entre clases.
+  diferentes clases hereden de otras.
+"""
+

59f0fa2b8c458b039dadd6afcce1ab7e8cce167d
CiberNefty <danieldbaproyects@gmail.com>
2023-01-08 10:21:42 -0500

    Modificamos el archivo main donde agregamos las primeras preguntas al usuario, main v1.1 -rama master

--- 20-proyecto-python/main.py
+++ 20-proyecto-python/main.py
@@ -12,3 +12,23 @@ PROYECTO Python y Mysql:
 - Si elegimos login, identifica al usuario y nos preguntara
 - Crear nota, mostrar notas, borrarlas.
 """
+
+print("""
+Acciones disponibles:
+    - Registro
+    - Login
+""")
+
+# Haremos una variable donde preguntara la accion que desee el usuario.
+accion = input('¿Que quiere hacer?: ')
+
+if accion == 'registro':
+    print("\nOk! Vamos a registrarte en el sistema...")
+    nombre = input('Cual es tu nombre?: ')
+    apellidos = input('Cuales son tus apellidos?: ')
+    mail = input('Introduce email: ')
+    password = input('Introduce tu contraseña: ')
+elif accion == 'login':
+    print("\nVale! Identificate en el sistema...")
+    mail = input('Introduce email: ')
+    password = input('Introduce tu contraseña: ')

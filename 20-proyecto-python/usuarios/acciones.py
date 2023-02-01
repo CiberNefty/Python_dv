a2fc36373007d6385ae474c2ee412b7ed131aa33
Daniel Vera <dsveraa.ing@gmail.com>
2023-02-01 16:41:26 -0500

    Acabamos de darle crear una forma para crear y registrar usuarios

--- 20-proyecto-python/usuarios/acciones.py
+++ 20-proyecto-python/usuarios/acciones.py
@@ -1,3 +1,5 @@
+import usuarios.usuario as modelo # Aqui le estamos dando un alias ya que es el modelo
+
 class Acciones:
 
     def registro(self):
@@ -7,6 +9,15 @@ class Acciones:
         apellidos = input('Cuales son tus apellidos?: ')
         mail = input('Introduce email: ')
         password = input('Introduce tu contraseña: ')
+        # Listo ahora tenemos que crear nuestro objeto que va a enviar los valores a nuestro modulo usuario
+        usuario = modelo.Usuario(nombre, apellidos, mail, password)
+        # Ahora tengo que registrar a mi usuario en la BD a los valores que le he pasado al objeto usuario.
+        registroUdb = usuario.registrar()
+
+        if registroUdb[0] >= 1: # Esta comprobacion esta evaluando lo que retorna el metodo registrar.
+            print(f"Perfecto {registroUdb[1].nombre}  te has registrado con el email {registroUdb[1].mail}")
+        else:
+            print("No te has registrado correctamente")
 
     def login(self):
         print("\nVale! Identificate en el sistema...")
--- 20-proyecto-python/usuarios/usuario.py
+++ 20-proyecto-python/usuarios/usuario.py
@@ -3,6 +3,7 @@ y que cada uno obtenga valores diferente y ademas me va a permitir reutilizar y 
 mas limpio mi codigo por que creare un objeto usuario que va a tener muchos metodos utilez.
 """
 import mysql.connector
+import datetime
 
 database = mysql.connector.connect(
     host='localhost',
@@ -24,8 +25,20 @@ class Usuario:
         self.password = password
 
     def registrar(self):
+        fecha = datetime.datetime.now() # estamos guardando la fecha completa 
     # Este metodo a parte de devolvernos nos mostrara el usuario registrado    
-        return self.nombre
+    # Este sera el metodo de registro de usuarios
+    # Lo primero que haremos es crear una variable sql para guardar la consulta.
+        sql = 'INSERT INTO usuarios VALUES (null,%s,%s,%s,%s,%s);'
+    # Los porcentajes eese(%s) nos sirve para sustituir datos que tengamos en una tapla.
+        usuario = (self.nombre, self.apellidos, self.mail, self.password, fecha)
+        cursor.execute(sql, usuario)
+        database.commit()
+        
+        # Vamos a devolver una lista de la cantidad de registros que se han modificado.
+        return [cursor.rowcount, self]
+        # Listo toca probarlo, ahora nos vamos al modulo acciones y tengo que importar el modelo de usuario que esta en el dicho paquete usuarios 
+
 
     def identificar(self):
     # Este metodo nos devolvera el objeto usuario que estemos identificando, en base a los datos que tenemos en las propiedades.

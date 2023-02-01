0b6283861ff5e6f956adc6eb1303f8ef59e2cd81
Daniel Vera <dsveraa.ing@gmail.com>
2023-02-01 17:28:55 -0500

    modificamos el archivo usuario donde importamos hashlib para cifrar la contraseña

--- 20-proyecto-python/usuarios/usuario.py
+++ 20-proyecto-python/usuarios/usuario.py
@@ -4,6 +4,7 @@ mas limpio mi codigo por que creare un objeto usuario que va a tener muchos meto
 """
 import mysql.connector
 import datetime
+import hashlib # importamos este modulo para poder cifrar las contraseñas que tengamos en texto plano en el motor de BD
 
 database = mysql.connector.connect(
     host='localhost',
@@ -13,7 +14,7 @@ database = mysql.connector.connect(
     database="proyectonotas_python"
 )
 #Verificamos si funciona la coneccion 
-#print(database)
+print(database)
 
 cursor = database.cursor(buffered=True) # Este buffered me va a permitir hacer muchas consultas usanndo el mismo cursor
 
@@ -28,10 +29,15 @@ class Usuario:
         fecha = datetime.datetime.now() # estamos guardando la fecha completa 
     # Este metodo a parte de devolvernos nos mostrara el usuario registrado    
     # Este sera el metodo de registro de usuarios
+
+    # Cifrar contraseña 
+        cifrado = hashlib.sha256()
+        cifrado.update(self.password.encode('utf8'))
+
     # Lo primero que haremos es crear una variable sql para guardar la consulta.
         sql = 'INSERT INTO usuarios VALUES (null,%s,%s,%s,%s,%s);'
     # Los porcentajes eese(%s) nos sirve para sustituir datos que tengamos en una tapla.
-        usuario = (self.nombre, self.apellidos, self.mail, self.password, fecha)
+        usuario = (self.nombre, self.apellidos, self.mail, cifrado.hexdigest(), fecha)
         try:
             cursor.execute(sql, usuario)
             database.commit()

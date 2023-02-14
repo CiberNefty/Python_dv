01526f181b674a68553137cac858fdf9938fc0fe
Daniel Vera <dsveraa.ing@gmail.com>
2023-02-14 11:46:41 -0500

    Pquete de notas y metodos para crear notas, v2.

---
+++ 20-proyecto-python/notas/acciones.py
@@ -0,0 +1,20 @@
+import notas.nota as modelo
+
+class Acciones:
+
+    def crear(self, usuario):
+        print(f"\nOk {usuario[1]}! Vamos a crear una nueva nota.")
+
+        # Vamos a sacar los datos que necesito para guardar la nota osea el usuario_id, el titulo, descripción.
+        titulo = input('Introduce el titulo de tu nota: ')
+        descripción = input('Mete el contenido de tu nota: ')
+        
+        # Listo ahora tengo que llamar a mi modelo, toca importarlo primero.
+        nota = modelo.Nota(usuario[0], titulo, descripción)
+        guardar = nota.guardar()
+
+        if guardar[0] >= 1:
+            print(f"\nPerfecto has guardado la nota: {nota.titulo}")
+        
+        else:
+            print(f"\nNo se ha guardado la nota, lo siento {usuario[1]}")

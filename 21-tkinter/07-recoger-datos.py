8e41aa8d2bce556bee252b44e05724afa87c4551
Daniel Vera <dsveraa.ing@gmail.com>
2023-03-03 22:18:14 -0500

    Agregamos una pequeña funcion para que obtuviera un dato de otra variable.

--- 21-tkinter/07-recoger-datos.py
+++ 21-tkinter/07-recoger-datos.py
@@ -1,6 +1,28 @@
 from tkinter import *
 
-ventana = Tk()
+ventana = Tk() 
+ventana.geometry('700x700')
+ventana.config(
+    bd=50, # Esto tendra un borde transparente de 50 pixeles
+)
 
+def getDato():
+    resultado.set(dato.get()) # Aquí le metemos el dato que esta dentro de dato
+
+dato = StringVar()
+resultado = StringVar()
+
+Label(ventana, text="Mete un texto: ").pack(anchor=NW)
+Entry(ventana, textvariable=dato).pack(anchor=NW) # Vamos a utilizar el objeto ENtry para meter un campo de texto, y textvariable es para recoger textos.
+
+Label(ventana, text="Dato recogido: ").pack(anchor=NW)
+texto_recogido = Label(ventana, textvariable=resultado)
+texto_recogido.config(
+    bg = 'green',
+    fg ='white'
+)
+texto_recogido.pack(anchor=NW)
+
+Button(ventana, text="Mostrar dato", command=getDato).pack(anchor=NW)
 
 ventana.mainloop()

7d0dfddae3c6e76d2d1a0d9e3f9b6ecfe8b56849
Daniel Vera <dsveraa.ing@gmail.com>
2023-03-07 16:35:43 -0500

    Modificamos la función de salir el cual recibe por parametro un valor y luego de validar el resultado de si queremos salir, nos envia un mensaje despidiendose con nuestro nombre y modificamos la linea 22 colocando la funcion lambda el cual se ejecutara cuando se le de clicl a ese boton, desplegando la funcion de Salir.

--- 21-tkinter/08-alertas.py
+++ 21-tkinter/08-alertas.py
@@ -12,11 +12,12 @@ def sacarAlerta():
 
 Button(ventana, text='Mostrar alerta!', command=sacarAlerta).pack() # Aqui estamos utilizando el comando que reconocera como metodo y colocamos el nombre de nuestra funcion sin los parentesis por que se ejecuta solo sin yo llamarlo.
 
-def salir():
+def salir(nombre):
     resultado = MessageBox.askquestion('Salir', '¿Quieres continuar ejecutando la aplicacion?')
 
     if resultado != 'yes':
+        MessageBox.showinfo('Good Bay', f'Adios {nombre}')
         ventana.destroy() # este metodo me permite cerrar la ventana.
 
-Button(ventana, text='Salir', command=salir).pack()
+Button(ventana, text='Salir', command= lambda: salir('Daniel Vera')).pack() # Cuando yo utilizo la funcion lambda en un comando se refiere es que caundo haga click en ell boton se ejecute la funcion.
 ventana.mainloop()

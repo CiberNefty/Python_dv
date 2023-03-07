efdd957f603c1cc968806d6f7953ec7d0321f669
Daniel Vera <dsveraa.ing@gmail.com>
2023-03-07 16:27:30 -0500

    Modificamos el archivo agregando dos funciones que utilizan el modulo de messagebox el cual nos da una alerta y la otra funcion utiliza el metodo askquestion para verificar si cerramos la aplicacion o no!.

--- 21-tkinter/08-alertas.py
+++ 21-tkinter/08-alertas.py
@@ -6,8 +6,17 @@ ventana.config(bd=70)
 
 def sacarAlerta():
 # Como crear alertas
-    MessageBox.showinfo('Alerta', 'Soy Daniel Vera')
+    MessageBox.showinfo('¡Alerta! Info', 'Soy Daniel Vera')
+    #MessageBox.showerror('¡Alerta! Error', 'Soy Daniel Vera')
+    #MessageBox.showwarning('¡Alerta! Warning', 'Soy Daniel Vera')
 
 Button(ventana, text='Mostrar alerta!', command=sacarAlerta).pack() # Aqui estamos utilizando el comando que reconocera como metodo y colocamos el nombre de nuestra funcion sin los parentesis por que se ejecuta solo sin yo llamarlo.
 
+def salir():
+    resultado = MessageBox.askquestion('Salir', '¿Quieres continuar ejecutando la aplicacion?')
+
+    if resultado != 'yes':
+        ventana.destroy() # este metodo me permite cerrar la ventana.
+
+Button(ventana, text='Salir', command=salir).pack()
 ventana.mainloop()

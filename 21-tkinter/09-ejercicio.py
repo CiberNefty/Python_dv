791bcbf0d03c737b92b91feea59786fc71907a9f
Daniel Vera <dsveraa.ing@gmail.com>
2023-03-07 17:00:19 -0500

    Modificamos el archivo agregando un borde, dos campos de texto y cuatro botones con posiciones de Anchor.

--- 21-tkinter/09-ejercicio.py
+++ 21-tkinter/09-ejercicio.py
@@ -9,5 +9,21 @@ from tkinter import messagebox as MessageBox
 
 ventana = Tk()
 ventana.title = "Ejercicio completo con Tkinter | DV"
+ventana.config(bd=25)
+
+num1 = StringVar()
+num2 = StringVar()
+resultado = StringVar()
+
+Label(ventana, text="Primer Numero: ").pack()
+Entry(ventana, textvariable=num1).pack()
+
+Label(ventana, text="Segundo Numero: ").pack()
+Entry(ventana, textvariable=num2).pack()
+
+Button(ventana, text='Suma', command=lambda:suma(num1, num2)).pack(anchor=NW)
+Button(ventana, text='Restar').pack(anchor=NE)
+Button(ventana, text='Multiplicar').pack(anchor=SW)
+Button(ventana, text='Dividir').pack(anchor=SE)
 
 ventana.mainloop()

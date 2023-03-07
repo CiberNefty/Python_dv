f826e52cc399cf2ba42e57d92995a1b2ababca3d
Daniel Vera <dsveraa.ing@gmail.com>
2023-03-07 16:16:54 -0500

    Creamos un boton y un metodo que a la hora de pulsar me creé una alerta utilizando el modulo messagebox.

--- 21-tkinter/08-alertas.py
+++ 21-tkinter/08-alertas.py
@@ -1,6 +1,13 @@
 from tkinter import *
-
+from tkinter import messagebox as MessageBox # Este modulo sirve para crear alertas.
 ventana =Tk()
-ventana.geometry('700x400')
+ventana.config(bd=70)
 
-Button(ventana, text='Mostrar alerta!')
+
+def sacarAlerta():
+# Como crear alertas
+    MessageBox.showinfo('Alerta', 'Soy Daniel Vera')
+
+Button(ventana, text='Mostrar alerta!', command=sacarAlerta).pack() # Aqui estamos utilizando el comando que reconocera como metodo y colocamos el nombre de nuestra funcion sin los parentesis por que se ejecuta solo sin yo llamarlo.
+
+ventana.mainloop()

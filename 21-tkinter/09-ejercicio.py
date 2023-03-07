1cccaa707f8c860b84dd9a69ea288af2da09f1bc
Daniel Vera <dsveraa.ing@gmail.com>
2023-03-07 16:50:24 -0500

    Creamos un archivo el cual se tratara de colocar en practica lo aprendido con tkinter realizando un ejercicio de calculadora.

---
+++ 21-tkinter/09-ejercicio.py
@@ -0,0 +1,13 @@
+"""
+CALCULADORA:
+- Dos campos de texto
+- 4 botones para las operacion
+- Mostrar el resultado
+"""
+from tkinter import *
+from tkinter import messagebox as MessageBox
+
+ventana = Tk()
+ventana.title = "Ejercicio completo con Tkinter | DV"
+
+ventana.mainloop()

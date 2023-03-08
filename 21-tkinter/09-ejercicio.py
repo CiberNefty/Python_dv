1f261c68113935b12c5b86fcda9f6604c6fbcec7
Daniel Vera <dsveraa.ing@gmail.com>
2023-03-07 21:47:42 -0500

    Hemos creado un marco para meter todo el formulario en ese marco.

--- 21-tkinter/09-ejercicio.py
+++ 21-tkinter/09-ejercicio.py
@@ -9,11 +9,20 @@ from tkinter import messagebox as MessageBox
 
 ventana = Tk()
 ventana.title = "Ejercicio completo con Tkinter | DV"
+ventana.geometry('400x400')
 ventana.config(bd=25)
 
 num1 = StringVar()
 num2 = StringVar()
 resultado = StringVar()
+
+# Vamos a crear un Frame (marco) para que todo este guardado en él.
+marco = Frame(ventana, width=250, height=250)
+marco.config(
+    bd=5,
+    relief= SOLID # Esto es un borde solido
+)
+marco.pack(side=TOP, anchor=CENTER)
 
 Label(ventana, text="Primer Numero: ").pack()
 Entry(ventana, textvariable=num1).pack()
@@ -21,9 +30,11 @@ Entry(ventana, textvariable=num1).pack()
 Label(ventana, text="Segundo Numero: ").pack()
 Entry(ventana, textvariable=num2).pack()
 
-Button(ventana, text='Suma', command=lambda:suma(num1, num2)).pack(anchor=NW)
-Button(ventana, text='Restar').pack(anchor=NE)
-Button(ventana, text='Multiplicar').pack(anchor=SW)
-Button(ventana, text='Dividir').pack(anchor=SE)
+label1 = Label(ventana, text="").pack()
+
+Button(ventana, text='Suma').pack(side='left')
+Button(ventana, text='Restar').pack(side='left')
+Button(ventana, text='Multiplicar').pack(side='left')
+Button(ventana, text='Dividir').pack(side='left')
 
 ventana.mainloop()

6cf1d9d25645d88b03c859b54329a2ab2feb7247
Daniel Vera <dsveraa.ing@gmail.com>
2023-03-07 22:06:58 -0500

    Modificamos el archivo logrando meter dentro del Frame todos los objetos del formulario,  acomodamos  objetos y agregamos un pack_propagate para que no se deforme el Frame.

--- 21-tkinter/09-ejercicio.py
+++ 21-tkinter/09-ejercicio.py
@@ -17,24 +17,29 @@ num2 = StringVar()
 resultado = StringVar()
 
 # Vamos a crear un Frame (marco) para que todo este guardado en él.
-marco = Frame(ventana, width=250, height=250)
+marco = Frame(ventana, width=340, height=200)
 marco.config(
+    padx=15,
+    pady=15,
     bd=5,
     relief= SOLID # Esto es un borde solido
 )
-marco.pack(side=TOP, anchor=CENTER)
+marco.pack(side=TOP, anchor=CENTER) 
+# Cuando metamos el formulario se va a deformar entonces tenemos que utlizar pack_propagate con valor de False para que no se deforme.
+# Entonce para meter el formulario en el Frame toca es en vez de ejecutar los label y los button en la ventana toca es meterlos al frame (marco).
+marco.pack_propagate(False)
 
-Label(ventana, text="Primer Numero: ").pack()
-Entry(ventana, textvariable=num1).pack()
+Label(marco, text="Primer Numero: ").pack()
+Entry(marco, textvariable=num1, justify='center').pack()
 
-Label(ventana, text="Segundo Numero: ").pack()
-Entry(ventana, textvariable=num2).pack()
+Label(marco, text="Segundo Numero: ").pack()
+Entry(marco, textvariable=num2,justify='center').pack()
 
-label1 = Label(ventana, text="").pack()
+label1 = Label(marco, text="").pack()
 
-Button(ventana, text='Suma').pack(side='left')
-Button(ventana, text='Restar').pack(side='left')
-Button(ventana, text='Multiplicar').pack(side='left')
-Button(ventana, text='Dividir').pack(side='left')
+Button(marco, text='Suma').pack(side='left',fill=X, expand=YES)
+Button(marco, text='Restar').pack(side='left',fill=X, expand=YES)
+Button(marco, text='Multiplicar').pack(side='left',fill=X, expand=YES)
+Button(marco, text='Dividir').pack(side='left',fill=X, expand=YES)
 
 ventana.mainloop()

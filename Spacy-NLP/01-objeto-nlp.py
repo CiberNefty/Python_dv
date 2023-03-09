7c616b58886a8afbf9370316cffbe5897fbbdf9a
Daniel Vera <dsveraa.ing@gmail.com>
2023-03-08 21:54:52 -0500

    Agregamos al archivo lo que seria sacar un indice de cada documento como token que vale en nlp

--- Spacy-NLP/01-objeto-nlp.py
+++ Spacy-NLP/01-objeto-nlp.py
@@ -8,5 +8,13 @@ nlp = spacy.blank('es')
 doc = nlp('¡Hello Word! daniel Vera')
 
 # Itera sobre los token en un Doc
-for token in doc:
-    print(token.text)
+"""for token in doc:
+    print(token.text)"""
+
+doc1 = nlp('¡Hola Mundo!')
+
+# Usa el indice del Doc para obtener un solo Token
+token = doc1[3]
+
+# Obten el texto del token a través del atributo .text
+print(token.text)

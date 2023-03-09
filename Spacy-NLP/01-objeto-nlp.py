48ae3762429fd6a5b892771746d0a9b61ac43f91
Daniel Vera <dsveraa.ing@gmail.com>
2023-03-08 21:48:26 -0500

    Creamos un doc para que al utilizar nuestro funcion nlp con caracteres al utilizarlo con for para iterar cada caracter y frase por separado

--- Spacy-NLP/01-objeto-nlp.py
+++ Spacy-NLP/01-objeto-nlp.py
@@ -4,4 +4,9 @@ import spacy
 # Creamos un objeto nlp vaio para procesar español
 nlp = spacy.blank('es')
 
+# Creado al procesar un string de texto con un objeto nlp
+doc = nlp('¡Hello Word! daniel Vera')
 
+# Itera sobre los token en un Doc
+for token in doc:
+    print(token.text)

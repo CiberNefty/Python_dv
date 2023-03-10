89cf75bf760735c26c0dd66fa55becc1b5e78cac
Daniel Vera <dsveraa.ing@gmail.com>
2023-03-09 23:11:05 -0500

    Modificamos el ejercicio procesando ingles y alamen.

--- Spacy-NLP/practica01/part2-ingles-aleman.py
+++ Spacy-NLP/practica01/part2-ingles-aleman.py
@@ -1,5 +1,15 @@
-"""PARTE 1: Ingles
+"""PARTE 1: Ingles - Aleman
 
-# Usa spacy.blank para crear un objeto nlp vacío para procesar inglés ("en").
+# Usa spacy.blank para crear un objeto nlp vacío para procesar inglés ("en" y 'de').
 # Crea un doc e imprime su texto en la pantalla.
 """
+import spacy
+
+#nlp = spacy.blank('en') # Ingles
+nlp = spacy.blank('de') # aleman
+
+doc = nlp("This is a sentence.")
+doc = nlp("Liebe Grüße!")
+
+print(doc.text)
+

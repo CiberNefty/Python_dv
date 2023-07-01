4fa53dc06882052bbda7f2bb4f23cff4fecabee1
Daniel Vera <dsveraa.ing@gmail.com>
2023-06-30 20:00:04 -0500

    Modificamos el archivo agregando un encabezado de tipo Label

--- 21-tkinter/11-formularios2.py
+++ 21-tkinter/11-formularios2.py
@@ -1,6 +1,21 @@
 from tkinter import *
 
 ventana = Tk()
+ventana.geometry('800x300')
+encabezado = Label(ventana, text='Formularios 2')#.pack()
+encabezado.config(
+    padx=15,
+    pady=15,
+    fg='white',
+    background='green',
+    font=('Consolas', 20)
+)#.pack()
+encabezado.pack(anchor=N, side=TOP,fill=X, expand=YES)
 
+# Botones Checkboxe
+
+# Radio buttons
+
+# Option Menu
 
 ventana.mainloop()

17308ebd06ef172d1301a5cd5831682523b0e344
Daniel Vera <dsveraa.ing@gmail.com>
2023-07-04 21:13:02 -0500

    Creamos un archivo el cual utilizaremos lo que hemos aprendido de archivos anteriores y crearemos un programa que contenga varias caracteristicas.

---
+++ 21-tkinter/13-proyecto.py
@@ -0,0 +1,12 @@
+"""
+Crear un programa que tenga:
+- Una ventana
+- Tamaño fijo
+- Ventana no redimensionable
+- Un Menu
+- Diferentes pantallas
+- Formulario de añadir productos
+- Guardar datos temporalmente
+- Mostrar datos listados de la pantalla home
+- Opcion de salir
+"""

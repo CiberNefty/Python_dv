bc84ba9713eaaad40471d05c050af4477cb725f9
Daniel Vera <dsveraa.ing@gmail.com>
2023-07-04 22:35:41 -0500

    Modificamos el archivo agregando varias pantallas utilizando metodos para llamarlas entre si, nos genera una pantalla sobre otra, tenemos que buscar una solucion.

--- 21-tkinter/13-proyecto.py
+++ 21-tkinter/13-proyecto.py
@@ -1,51 +1,80 @@
 """
 Crear un programa que tenga:
-- Una ventana
-- Tamaño fijo
-- Ventana no redimensionable
-- Un Menu
+- (hecho) Una ventana
+- (hecho) Tamaño fijo
+- (hecho) Ventana no redimensionable [funcion: resizable()]
+- (hecho) Un Menu (Inicio, Añadir, Informacion, Salir)
+- (hecho) Opcion de salir
 - Diferentes pantallas
 - Formulario de añadir productos
 - Guardar datos temporalmente
 - Mostrar datos listados de la pantalla home
-- Opcion de salir
+
 """
 from tkinter import *
 
+# Definimos ventana
 ventana = Tk()
-ventana.geometry('600x500')
-ventana.title('Programa con Tkinter')
+ventana.geometry('500x500')
+ventana.title('Programa Tkinter - Python DV')
+ventana.resizable(False,False) # O puede ser  (0,0)
 
-mi_menu = Menu(ventana)
-ventana.config(menu=mi_menu)
-
-# Crearemos un submenu.
-archivo = Menu(mi_menu, tearoff= 0)
-archivo.add_command(label='Nuevo archivo de texto')
-archivo.add_command(label='Nuevo archivo')
-archivo.add_command(label='Nueva ventana')
-
-editar = Menu(mi_menu, tearoff = 0)
-editar.add_command(label='Deshacer')
-editar.add_command(label='Rehacer')
-editar.add_separator()
-editar.add_command(label='Cortar')
-# Submenu para editar
-subEditar = Menu(editar, tearoff= 0)
-subEditar.add_command(label='Buscar')
-subEditar.add_command(label='Remplazar')
-
-editar.add_cascade(label='Configuracion', menu= subEditar)
-editar.add_command(label='Pegar')
-
-seleccion = Menu(mi_menu, tearoff = 0)
-seleccion.add_command(label='Seleccionar todo')
-seleccion.add_command(label='Expandir seleccion')
-seleccion.add_command(label='Reducir Seleccion')
-
-mi_menu.add_cascade(label='Archivo', menu=archivo)
-mi_menu.add_cascade(label='Editar', menu = editar)
-mi_menu.add_cascade(label='Seleccion', menu = seleccion)
+# PANTALLAS
+# van hacer metodos el los cuales me mostrara otro tipo de pantallas
+def home(): 
+    home_label = Label(ventana, text='INICIO')
+    home_label.config(
+        foreground= 'white',
+        background= 'Black',
+        font= ('Arial', 20,'bold'),
+        padx=20,
+        pady=20
+    )
+    home_label.grid(row=0, column=0)
 
 
+    return True
+
+def add(): 
+    add_label = Label(ventana, text='AÑADIR PRODUCTO')
+    add_label.config(
+        foreground= 'white',
+        background= 'Black',
+        font= ('Arial', 20,'bold'),
+        padx=20,
+        pady=20
+    )
+    add_label.grid(row=0, column=0)
+
+    return True
+
+def info():
+    info_label = Label(ventana, text='INFORMACION')
+    info_label.config(
+        foreground= 'white',
+        background= 'Black',
+        font= ('Arial', 20,'bold'),
+        padx=20,
+        pady=20
+    )
+    info_label.grid(row=0, column=0)
+
+    data_label = Label(ventana, text='Creado por Daniel Vera en 06-2023')
+    data_label.grid(row=1, column=0)
+    return True
+
+# Cargamos la pantalla de inicio
+home()
+
+# Definimos el Menu superior
+menu_Padre = Menu(ventana)
+menu_Padre.add_cascade(label='Inicio', command=home)
+menu_Padre.add_cascade(label='Añadir', command= add)
+menu_Padre.add_cascade(label='Información', command= info)
+menu_Padre.add_command(label='Salir', command = quit)
+
+# Cargar Menu
+ventana.config(menu = menu_Padre)
+
+# Cargamos ventana para que abra.
 ventana.mainloop()

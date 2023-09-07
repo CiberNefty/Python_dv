8420cffdebb1dcc95a6e6f052991d3d32af4090a
Daniel Vera <dsveraa.ing@gmail.com>
2023-09-06 22:15:54 -0500

    Agregamos varias funciones para utlizar con el QLineEdit.

--- Proyectos/2023/PyQt6-Inicios/06-QLineEdit.py
+++ Proyectos/2023/PyQt6-Inicios/06-QLineEdit.py
@@ -3,7 +3,8 @@ from PyQt6.QtWidgets import (
     QApplication,
     QWidget,
     QLineEdit,
-    QVBoxLayout
+    QVBoxLayout,
+    QCompleter
 )
 
 
@@ -14,20 +15,41 @@ class MainWindow(QWidget):
         self.setWindowTitle('PyQt QLineEdit')
         self.setGeometry(100, 100, 320, 210)
 
+        """CAJA DE TEXTO CON BOTON DE LIMPIAR"""
         cajabusqueda = QLineEdit(
             self,
             placeholderText = 'Escribe aquí',
             clearButtonEnabled = True
         )
-
+        
+        """CAJA DE TEXTO EN PASSWORD"""
         password = QLineEdit(self,
             echoMode = QLineEdit.EchoMode.Password,
-            clearButtonEnabled = True
+            clearButtonEnabled = True,
+            placeholderText = 'Escribe tu contraseña',
+        )
+
+        """CAJA DE TEXTO CON AUTOCOMPLETAR"""
+        texto_autocompletar = QCompleter(['Manzana','Pera','Banano','Mora','Limon','Sandia','Naranja','Fresa','Curuva', 'Papaya', 'Mandarina','Durazno','Uva','Melocoton'])
+
+        frutas = QLineEdit(
+            self,
+            placeholderText='Escribe Frutas'
+        )
+        frutas.setCompleter(texto_autocompletar)
+
+        """CAJA PARA MAXIMO DE CARACTERES"""
+        maximoCaracter = QLineEdit(
+            self,
+            placeholderText='Limite de 8 caracteres',
+            maxLength=8
         )
 
         layout = QVBoxLayout()
         layout.addWidget(cajabusqueda)
         layout.addWidget(password) # Aqui agregamos nuestra QlineEdit con password
+        layout.addWidget(frutas)
+        layout.addWidget(maximoCaracter)
 
         self.setLayout(layout)
         self.show()

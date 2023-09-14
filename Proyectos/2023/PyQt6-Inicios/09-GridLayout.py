38c5a1748757324ab358c34d7b22f558a1151fa2
Daniel Vera <dsveraa.ing@gmail.com>
2023-09-14 18:06:16 -0500

    Utilizamos el metodo o la funcion de QGridLayout para hacer un pequeño formulario sobre un inisio de sesion, utilizamos un desglose de dos alternativas de como colocar los objetos, dandole una determinada posicion con el gridLayout.

---
+++ Proyectos/2023/PyQt6-Inicios/09-GridLayout.py
@@ -0,0 +1,57 @@
+import sys
+from PyQt6.QtWidgets import QWidget, QApplication, QPushButton, QLabel, QGridLayout, QLineEdit
+# Para obtener el valor o (metodo) de aliniamiento toca importar el QtCore
+from PyQt6.QtCore import Qt
+
+class MainWindow(QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.setWindowTitle('Grid Layout')
+        #self.setGeometry(100, 100, 100, 300)
+
+        layoutPadre_Grid = QGridLayout()
+        self.setLayout(layoutPadre_Grid)
+
+        # USUARIO
+        """user_input = QLineEdit(self,
+            placeholderText = 'Escribe tu nombre aquí...',
+            clearButtonEnabled = True
+        )"""
+
+        layoutPadre_Grid.addWidget(QLabel('Usuario: '),0,0)
+        layoutPadre_Grid.addWidget(QLineEdit(self, 
+            placeholderText = 'Escribe tu usuario...',
+            clearButtonEnabled = True),
+            0,1)
+        
+        # CONTRASEÑA
+        contraseña_label = QLabel('Contraseña: ')
+        layoutPadre_Grid.addWidget(contraseña_label, 1,0)
+        layoutPadre_Grid.addWidget(QLineEdit(
+            self, placeholderText = '*****************',
+            echoMode = QLineEdit.EchoMode.Password,
+            clearButtonEnabled = True
+        ),1,1) # Position the QlineEdit
+
+        # Botones
+        """boton1 = QPushButton()
+        boton1.setText("Boton 1ds")
+        #boton1.setFixedSize(100,30)
+        #boton1.setIcon('') # Toca volver a retomar la libreria de IO
+        boton1.setStyleSheet('QPushButton{background-color: GREY}')
+
+        layoutPadre_Grid.addWidget(boton1, 3,0, 2,0, Qt.AlignmentFlag.AlignBottom)"""
+        layoutPadre_Grid.addWidget(QPushButton('Iniciar'), 2,0,
+                                    Qt.AlignmentFlag.AlignHCenter)
+        layoutPadre_Grid.addWidget(QPushButton('Cerrar'), 2,1,
+                                   alignment=Qt.AlignmentFlag.AlignHCenter)
+        
+
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ventana = MainWindow()
+    sys.exit(app.exec())

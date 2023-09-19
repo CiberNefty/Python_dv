e65df15ae5eab65a5cbe4c400e5dfc6feec61b8e
Daniel Vera <dsveraa.ing@gmail.com>
2023-09-18 21:44:17 -0500

    Modificamos el archivo para agregar una clase que es para validar si el estado de cambio se utiliza o no! y usamos una señal para conectar y evaluarla dentro de una funcion aparte.

--- Proyectos/2023/PyQt6-Inicios/11-QCheckBox.py
+++ Proyectos/2023/PyQt6-Inicios/11-QCheckBox.py
@@ -12,12 +12,25 @@ class VentanaQCheckBox(QWidget):
         self.setLayout(layoutPadre)
 
         # Creamos nuestro checkBox
-        checkBox1 = QCheckBox('I gree', self)
+        checkBox1 = QCheckBox('Ya estoy aqui', self)
+        checkBox1.stateChanged.connect(self.on_checkbox_changed) # Estado de cambio metodo (stateChange) con una señal.
+
         # Agregamos nuestro checkBox a nuestro gridlayout y lo posicionamos y le damos un aliniamiento.
         layoutPadre.addWidget(checkBox1, 0,0, Qt.AlignmentFlag.AlignBottom)
         
 
         self.show()
+
+    def on_checkbox_changed(self, checkBox1):
+        estado = Qt.CheckState(checkBox1)
+        
+        if estado == Qt.CheckState.Checked:
+            print("Checkiado - Comprobado")
+        elif estado == Qt.CheckState.Unchecked:
+            print("Unchecked - Desenfrenado - No comprobado")
+        elif estado == Qt.CheckState.PartiallyChecked: # Esta condicion no me la evalua porque no veo que utilice mas de dos checkpoints
+            print("PartiallyChecked - Pacialmente comprobado")
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)

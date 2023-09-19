1b7101b56279fb6838abfdfbaab911c6b8a50605
Daniel Vera <dsveraa.ing@gmail.com>
2023-09-19 15:36:38 -0500

    Modificamos el archivo el cual agregamos un objeto de la clase QSpinBox y lo agregamos a un QVBoxLayout. este es como un tipo de rueda que nos permite agregar contenido manutal o de forma incremental.

--- Proyectos/2023/PyQt6-Inicios/14-QSpinBox.py
+++ Proyectos/2023/PyQt6-Inicios/14-QSpinBox.py
@@ -11,7 +11,23 @@ class VentanaPadre(QWidget):
         layoutPadre = QVBoxLayout()
         self.setLayout(layoutPadre)
 
+        # Creamos nuestro SpinBox
+        spinBox_amount = QSpinBox(
+            minimum = 1, maximum = 50, value= 20, prefix = '$'
+        )
+        # Creamos una conexion con el metodo valueChanged.connect
+        spinBox_amount.valueChanged.connect(self.funcionActualizacion)
+
+        self.resultado = QLabel('',self)
+
+        # Agregamos los objetos al layout
+        layoutPadre.addWidget(spinBox_amount)
+        layoutPadre.addWidget(self.resultado)
+
         self.show()
+
+    def funcionActualizacion(self, valor):
+        self.resultado.setText(f'Valor acutal: {valor}')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)

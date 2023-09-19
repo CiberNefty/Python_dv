c986667d6d6bf8785154d4b55bd9e66cd63acb4f
Daniel Vera <dsveraa.ing@gmail.com>
2023-09-18 22:36:35 -0500

    Modificamos el archivo agregando un GridLayout luego creamos nuestro checBox luego creamos dos botones que son los que van a cambiar el estado de el checkBox al darle el evento clicked que estan conectadas a dos funciones a parte de la principal.

--- Proyectos/2023/PyQt6-Inicios/11.1-QCheckBoxSetChanged.py
+++ Proyectos/2023/PyQt6-Inicios/11.1-QCheckBoxSetChanged.py
@@ -9,8 +9,34 @@ class VentanaQCheck_Stados (QWidget):
         self.setWindowTitle('Metodo Que permite cambiar de estado (TRUE or FALSE)')
         self.setGeometry(100,100,100,100)
 
+        # Creamos nuestro caja de guardado (GridLayout)
+        gridLayoutPadre = QGridLayout()
+        self.setLayout(gridLayoutPadre)
+    
+        # Creamos un checBox
+        self.checkBox1 = QCheckBox('Cambio de estado', self)
+
+        # Creamos dos botones que nos serviran para cambiar de estado (TRUE or FALSE)
+        Boton1 = QPushButton('CheckearBoton', self)
+        # Vamos a darle una bandera y llevarla a una señal
+        Boton1.clicked.connect(self.chekiado)
+
+        Boton2 = QPushButton('Quitar Checkeable', self)
+        Boton2.clicked.connect(self.noChekiado)
+
+        # Agregamos los objetos al layout
+        gridLayoutPadre.addWidget(self.checkBox1, 0,0,0,2, Qt.AlignmentFlag.AlignCenter)
+        gridLayoutPadre.addWidget(Boton1, 1,0, Qt.AlignmentFlag.AlignJustify)
+        gridLayoutPadre.addWidget(Boton2, 1,1, Qt.AlignmentFlag.AlignJustify)
 
         self.show()
+
+    def chekiado(self): # CUando solo colocamos una self solo envia un valor.
+        self.checkBox1.setChecked(True)
+
+    def noChekiado(self):
+        self.checkBox1.setChecked(False)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)

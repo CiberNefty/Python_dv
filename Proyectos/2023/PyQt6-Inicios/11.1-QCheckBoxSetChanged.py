ea39ba5e15dfb2f235ef04e4066f2a638297f6b5
Daniel Vera <dsveraa.ing@gmail.com>
2023-09-18 22:50:58 -0500

    Agregamos un checkBox con el metodo setTristate( ) el cual siver como una inparcialidad de los dos estados.

--- Proyectos/2023/PyQt6-Inicios/11.1-QCheckBoxSetChanged.py
+++ Proyectos/2023/PyQt6-Inicios/11.1-QCheckBoxSetChanged.py
@@ -24,10 +24,16 @@ class VentanaQCheck_Stados (QWidget):
         Boton2 = QPushButton('Quitar Checkeable', self)
         Boton2.clicked.connect(self.noChekiado)
 
+        # 2° Creamos un de verificacion Triestatal
+        """En la práctica, se utiliza una casilla de verificación triestatal para dar al usuario la opción de no marcar ni desmarcar la casilla de verificación."""
+        self.checkBoxTristatal = QCheckBox('Un CheckBox Tristatal', self)
+        self.checkBoxTristatal.setTristate(True)
+
         # Agregamos los objetos al layout
-        gridLayoutPadre.addWidget(self.checkBox1, 0,0,0,2, Qt.AlignmentFlag.AlignCenter)
+        gridLayoutPadre.addWidget(self.checkBox1, 0,0,1,2, Qt.AlignmentFlag.AlignCenter)
         gridLayoutPadre.addWidget(Boton1, 1,0, Qt.AlignmentFlag.AlignJustify)
         gridLayoutPadre.addWidget(Boton2, 1,1, Qt.AlignmentFlag.AlignJustify)
+        gridLayoutPadre.addWidget(self.checkBoxTristatal, 2,0,2,0, Qt.AlignmentFlag.AlignCenter)
 
         self.show()
 
@@ -42,3 +48,12 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     ventana = VentanaQCheck_Stados()
     sys.exit(app.exec())
+
+
+"""
+Resumen
+ - Utilice la clase para crear un widget de casilla de verificación. 'QCheckbox'
+ - La señal se emite cuando la casilla de verificación está marcada o desmarcada. 'stateChanged'
+ - Utilice el método or para activar o desactivar una casilla de verificación mediante programación. 'setChecked()' 'setState()'
+ - Utilice el método para crear una casilla de verificación triestatal. 'setTristate()'
+"""

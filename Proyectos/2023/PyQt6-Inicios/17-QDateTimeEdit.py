2935910f73b532cbed1701645409a9b2e7c44bde
Daniel Vera <dsveraa.ing@gmail.com>
2023-09-22 18:46:05 -0500

    Modificamos el archivo agregando el objeto QDateTimeEdit el cual nos sirve como editable de fechas.

--- Proyectos/2023/PyQt6-Inicios/17-QDateTimeEdit.py
+++ Proyectos/2023/PyQt6-Inicios/17-QDateTimeEdit.py
@@ -1,14 +1,32 @@
 import sys
-from PyQt6.QtWidgets import QWidget, QApplication, QFormLayout, QLabel
+from PyQt6.QtWidgets import QWidget, QApplication, QFormLayout, QLabel, QDateTimeEdit
 
 class VentanaPadre(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.setWindowTitle('PyQt QDateEdit')
+        self.setWindowTitle('QDateTimeEdit')
+        #self.setGeometry(100,100,200,100)
+        self.setMinimumWidth(200)
 
+        layoutPadre_form = QFormLayout()
+        self.setLayout(layoutPadre_form)
+
+        self.fecha_tiempo_editable = QDateTimeEdit(self, calendarPopup = True)
+        self.fecha_tiempo_editable.dateTimeChanged.connect(self.tiempo)
+
+        self.resultado_tiempo_editable = QLabel('',self)
+
+        layoutPadre_form.addRow('Date: ', self.fecha_tiempo_editable)
+        layoutPadre_form.addRow(self.resultado_tiempo_editable)
+
+        self.show()
+
+    def tiempo(self):
+        valorRecogido = self.fecha_tiempo_editable.dateTime()
+        self.resultado_tiempo_editable.setText(valorRecogido.toString("yyyy-MM-dd HH:mm"))
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = VentanaPadre()
+    ventana = VentanaPadre()
     sys.exit(app.exec())

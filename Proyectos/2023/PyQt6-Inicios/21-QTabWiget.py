57a29b68742b56fa9afc2c214e24fe576ecfdc64
Daniel Vera <dsveraa.ing@gmail.com>
2023-09-24 20:14:53 -0500

    Modificamos el archivo para visualizar lo que seria otra forma de escribir codigo "limpio" el cual agregamos y utilizamos objetos de la clase QTabWidget, QWidget, QLineEdit, QDateEdit, QPushButton el cual los organizamos dentro de un GridLayout.

--- Proyectos/2023/PyQt6-Inicios/21-QTabWiget.py
+++ Proyectos/2023/PyQt6-Inicios/21-QTabWiget.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt6.QtWidgets import QWidget, QApplication, QLineEdit, QHBoxLayout, QGridLayout, QTabWidget, QDateEdit, QPushButton
+from PyQt6.QtWidgets import QWidget, QApplication, QLineEdit, QHBoxLayout, QGridLayout, QTabWidget, QDateEdit, QPushButton, QFormLayout
 from PyQt6.QtCore import Qt
 
 class ventanaPadre(QWidget):
@@ -14,22 +14,41 @@ class ventanaPadre(QWidget):
         self.setLayout(Layoutpadre)
 
         # Grupo de Pestañas 1
+        tab = QTabWidget(self)
         
-         
         # Cramos primero nuestras pestaña 1 del grupo de pestañas 1
+        # Panel de informacion persona
+        pagina_personal = QWidget(self)
+        layoutPagePerson = QFormLayout()
+        pagina_personal.setLayout(layoutPagePerson)
         
-
+        # Agregamos objetos a el estilo de la hoja (pestaña 1 - QGridLayout) (QFormLayout)
+        layoutPagePerson.addRow('Nombres: ', QLineEdit(self))
+        layoutPagePerson.addRow('Apellidos: ', QLineEdit(self))
+        layoutPagePerson.addRow('DOB: ', QDateEdit(self))
         
-        # Agregamos objetos de escrituras a el estilo de la hoja (pestaña) (Gridlayout)
-        
-        
-
         # Cramos primero nuestras pestaña 2
-              
+        # Panel de informacion de contacto
+        pagina_contacto = QWidget(self)
+        layoutPageContact = QFormLayout()
+        pagina_contacto.setLayout(layoutPageContact)
         
+        # Agregamos objetos a el estilo de la hoja (pestaña 2 - QGridLayout) (QFormLayout)
+        layoutPageContact.addRow('Numero Telefonico: ', QLineEdit(self))
+        layoutPageContact.addRow('Contacto de informacion: ', QLineEdit(self))
+
+        # Añadimos al tab (grupo de pestañas) cada pagina contenedora de widgets y su nombre
+        tab.addTab(pagina_personal, 'Informacion Personal')
+        tab.addTab(pagina_contacto, 'Informacion de Contacto')
+
+        # Añadimos los paneles de paginas a la clase de objeto de QTabWidget (Grupo de pestañas 1)        
+        Layoutpadre.addWidget(tab, 0,0,2, 1)
+        Layoutpadre.addWidget(QPushButton('Guardar'), 2,0,
+                              alignment= Qt.AlignmentFlag.AlignLeft)
+        Layoutpadre.addWidget(QPushButton('Cancel'), 2,0,
+                              alignment= Qt.AlignmentFlag.AlignRight)
         
 
-        # Añadimos los grupos de pestañas a la hoja principal 1
         
         self.show()
 

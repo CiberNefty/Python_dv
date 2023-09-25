611f9bdc851a184b366f62c4f891d1bdd9a07f39
Daniel Vera <dsveraa.ing@gmail.com>
2023-09-24 19:42:08 -0500

    Utilizamos el archivo el cual agregamos y dimos fubncionalidad para agregar varios grupo de pestañas a parte con GridLayout.

--- Proyectos/2023/PyQt6-Inicios/20-QTabWidget.py
+++ Proyectos/2023/PyQt6-Inicios/20-QTabWidget.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt6.QtWidgets import QWidget, QApplication, QLineEdit, QFormLayout, QHBoxLayout
+from PyQt6.QtWidgets import QWidget, QApplication, QLineEdit, QHBoxLayout, QGridLayout, QTabWidget, QDateEdit, QPushButton
+from PyQt6.QtCore import Qt
 
 class ventanaPadre(QWidget):
     def __init__(self, *args, **kwargs):
@@ -8,6 +9,47 @@ class ventanaPadre(QWidget):
         self.setWindowTitle("QTabWidget")
         #self.setGeometry(100,100,100,100)
 
+        # Creamos Nuestra hoja Principal
+        LayoutMain = QHBoxLayout()
+        self.setLayout(LayoutMain)
+
+        # Grupo de Pestañas 1
+        tab = QTabWidget()
+        LayoutMain.addWidget(tab)
+         
+        # Cramos primero nuestras pestaña 1 del grupo de pestañas 1
+        page1 = QWidget(tab) # Contenedor
+        page_layoutGrid1 = QGridLayout()
+        page1.setLayout(page_layoutGrid1)
+        
+        # Agregamos objetos de escrituras a el estilo de la hoja (pestaña) (Gridlayout)
+        page_layoutGrid1.addWidget(QLineEdit(page1),0,0, Qt.AlignmentFlag.AlignCenter)
+        
+
+        # Cramos primero nuestras pestaña 2
+        page2 = QWidget(tab)
+        page_layoutGrid2 = QGridLayout()
+        page2.setLayout(page_layoutGrid2)        
+        
+        # Grupo de Pestañas 2
+        tab1 = QTabWidget()
+        LayoutMain.addWidget(tab1)
+
+        # Cramos primero nuestras pestaña 1
+        page3 = QWidget(tab1)
+        page_layoutGrid3 = QGridLayout()
+        page3.setLayout(page_layoutGrid3)
+
+        # Cramos primero nuestras pestaña 2
+        page4 =QWidget(tab1)
+        page_layoutGrid4 = QGridLayout()
+        page4.setLayout(page_layoutGrid4)
+
+        # Añadimos los grupos de pestañas a la hoja principal 1
+        tab.addTab(page1, 'Hoja 1')
+        tab.addTab(page2, 'Hoja 2')
+        tab1.addTab(page3, 'Hoja 3')
+        tab1.addTab(page4, 'Hoja 4')
 
         self.show()
 

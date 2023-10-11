3e0faa5be88528043d645c1ea60f7c49bc4ab7fb
Daniel Vera <dsveraa.ing@gmail.com>
2023-10-10 21:55:10 -0500

    Creamos un pequeño formulario el cual utilizamos un cuadro de grupo para meter dentro varios objetos, la idea fue darle una presentacion mas organizada entre formularios.

--- Proyectos/2023/PyQt6-Inicios/22-QGroupBox.py
+++ Proyectos/2023/PyQt6-Inicios/22-QGroupBox.py
@@ -1,12 +1,37 @@
 import sys
-from PyQt6.QtWidgets import QWidget, QApplication
+from PyQt6.QtWidgets import QWidget, QApplication, QGroupBox, QFormLayout, QPushButton, QLineEdit, QDateEdit
 
 class clasePadre(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.setWindowTitle('QGroupBox')
-        self.setGeometry(100,100,100,100)
+        #self.setGeometry(100,100,200,200)
+        
+        # creamos nuestro layou principal donde va ir guardado el groupBox1
+        layout_padre = QFormLayout()
+        self.setLayout(layout_padre)
+
+        # Creamos nuestro Cuadro de Grupo QgroupBox para INFORMACION DE PERSONAS
+        person_groupbox = QGroupBox('Informacion Personal')
+        form_layout = QFormLayout()
+        person_groupbox.setLayout(form_layout)
+
+        form_layout.addRow('Nombres: ', QLineEdit(person_groupbox))
+        form_layout.addRow('Apellidos: ', QLineEdit(person_groupbox))
+        form_layout.addRow('DOB', QDateEdit())
+
+        # Creamos nuestro Cuadro de Grupo QgroupBox para CONTACTOS
+        contacto_groupbox = QGroupBox('Contacto de Informacion')
+        form_layout = QFormLayout()
+        contacto_groupbox.setLayout(form_layout)
+
+        form_layout.addRow('Numero Telefonico: ', QLineEdit(contacto_groupbox))
+        form_layout.addRow('Correo Electronico: ', QLineEdit(contacto_groupbox))
+
+        layout_padre.addRow(person_groupbox)
+        layout_padre.addRow(contacto_groupbox)
+        layout_padre.addRow(QPushButton('SAVE'))
 
 
         self.show()

d6eafd03cc01ec03bb3a29f133bfdfbc7343e863
Daniel Santiago Vera Arias <dsveraa.ing@gmail.com>
2026-07-01 20:16:13 -0500

    Emepezamos a repasar conceptos de creacion de  modelos y ejecucion de migraciones a una sqlite.

--- 22-django/AprendiendoDjango/miapp/models.py
+++ 22-django/AprendiendoDjango/miapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=110)
+    content = models.TextField()
+    image = models.ImageField(default='null')
+    public = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at= models.DateTimeField(auto_now=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=260)
+    created_at = models.DateField()
+
+"""
+Cuando ya tenemos nuestro modelo o hacemos algun cambio a 
+nuestra estructura de tablas requermos hacer migraciones si o si.
+Entrando a la ruta del proyecto y ejecutar en consola:
+$ Ruta_proyecto>python manage.py makemigrations
+Luego ejecutar para sql
+$ python manage.py migrate nombre_app nombre_migracion
+Luego toca ejecutar y migrar todo esto para que se guarde en la db
+$ python manage.py migrate
+"""

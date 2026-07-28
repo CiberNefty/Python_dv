0e68140a85a33179fd09eb677a5a2ad9deff7103
Daniel Santiago Vera Arias <dsveraa.ing@gmail.com>
2026-07-28 16:47:43 -0500

    Feat: Trabajamos validando los datos del formulario (validator-forms).

--- 22-django/AprendiendoDjango/miapp/forms.py
+++ 22-django/AprendiendoDjango/miapp/forms.py
@@ -1,22 +1,31 @@
 from django import forms
+from django.core import validators
 
 class FormularioArticle(forms.Form):
 
     title = forms.CharField(
         label="Titulo",
         max_length=40,
-        required=False,
+        required=True,
         widget= forms.TextInput(
             attrs={
                 'placeholder':'Mete el titulo',
                 'class': 'titulo_form_article'
             }
-        )
+        ),
+        validators=[
+            validators.MinLengthValidator(4, "El titulo es demasiado corto"),
+            validators.RegexValidator('^[A-Za-z0-9ñ ]*$', "El titulo esta mal formado",'invalid_title') # Solo letra y numero 
+        ]
     )
 
     content = forms.CharField(
         label = "Contenido",
-        widget= forms.Textarea
+        widget= forms.Textarea,
+        validators = [
+            validators.MaxLengthValidator(20, 'Te has pasado de caracteres maximo 20 caracteres de contenido'),
+            
+        ]
     )
     content.widget.attrs.update({
         'placeholder':'Mete el contenido del articulo completo',

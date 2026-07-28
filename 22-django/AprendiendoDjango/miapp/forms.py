2c1e62ae9ef0d3d1cb3c45618d24bf3872a2b7dc
Daniel Santiago Vera Arias <dsveraa.ing@gmail.com>
2026-07-28 12:59:58 -0500

    Feat: Perzonalizamos el formulario.

--- 22-django/AprendiendoDjango/miapp/forms.py
+++ 22-django/AprendiendoDjango/miapp/forms.py
@@ -3,13 +3,26 @@ from django import forms
 class FormularioArticle(forms.Form):
 
     title = forms.CharField(
-        label="Titulo"
+        label="Titulo",
+        max_length=40,
+        required=False,
+        widget= forms.TextInput(
+            attrs={
+                'placeholder':'Mete el titulo',
+                'class': 'titulo_form_article'
+            }
+        )
     )
 
     content = forms.CharField(
         label = "Contenido",
         widget= forms.Textarea
     )
+    content.widget.attrs.update({
+        'placeholder':'Mete el contenido del articulo completo',
+        'class': 'contenido_form_article',
+        'id':'conteido_form_id',
+    })
 
     public_options = [
         (1, "Si"),
